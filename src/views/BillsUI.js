e3d3e4b282f06cda50194c493e62af33c084f340
sedomu <seguin@lilo.org>
2025-04-21 16:27:21 +0200

    Fixing: [Bug report] - Bills

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,7 +20,11 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+    let sortedData = data.sort((a,b) => {
+        return new Date(b.date) - new Date(a.date);
+    })
+
+  return (sortedData && sortedData.length) ? sortedData.map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
